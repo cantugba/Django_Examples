257577a50afb691f5a320045295218b9ef2d0a58
tugbaca <tugbacan57@outlook.com>
2020-10-04 22:33:50 +0300

    movies url eklendi

--- MoviesCatalog/catalog/catalog/urls.py
+++ MoviesCatalog/catalog/catalog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path ,include
 #http://127.0.0.1:8000/
 urlpatterns = [
     path('',include('pages.urls')),
+    path('movies/', include('movies.urls')),
     path('admin/', admin.site.urls),
 
 ]
