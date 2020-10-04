2600f135d54cd644f513575bc04f8fd80f696e49
tugbaca <tugbacan57@outlook.com>
2020-10-04 22:32:38 +0300

    movies path eklendi

--- MoviesCatalog/catalog/catalog/settings.py
+++ MoviesCatalog/catalog/catalog/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'movies.apps.MoviesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
