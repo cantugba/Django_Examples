735ce802dc25135019379ae18ead759b0af0d13a
tugbaca <tugbacan57@outlook.com>
2020-10-08 12:44:29 +0300

    csrf token added

--- MoviesCatalog/catalog/user/views.py
+++ MoviesCatalog/catalog/user/views.py
@@ -5,7 +5,10 @@ def login(request):
     return render(request,'user/login.html')
 
 def register(request):
-    return render(request,'user/register.html')
+    if request.method == 'POST':
+        print('submitted')
+    else:
+        return render(request,'user/register.html')
 
 def logout(request):
     return render(request,'user/logout.html')
