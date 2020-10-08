e0234f23cb4562393c4941f9f97b76c180c6a9d5
tugbaca <tugbacan57@outlook.com>
2020-10-08 14:01:56 +0300

    login işlemleri için session id oluşturuldu

--- MoviesCatalog/catalog/user/views.py
+++ MoviesCatalog/catalog/user/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib import auth #gönderilen bilgilerle eşleşen kayıtlaarın varlığının sorgulamasını yapıyor
 # Create your views here.
 def login(request):
-    return render(request,'user/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user= auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request,user)
+            print('login başarılı')
+            return redirect('index')
+        else:
+            return redirect('login')
+            
+    else:
+        return render(request,'user/login.html')
 
 def register(request):
     if request.method == 'POST':
